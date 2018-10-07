7ea2a144c49e936c99b6fb5ea21c562bac8ab20e
apple@elephant.loans <apple@elephant.loans>
2018-10-07 10:49:20 +0700

    Fix typo

--- algorithms/mathematic/greatest-common_devider/main.go
+++ algorithms/mathematic/greatest-common_devider/main.go
@@ -1,19 +1,19 @@
 package main
 
-// concept: let's say commond devider is x and n1 >= n2
+// concept: let's say commond divider is x and n1 >= n2
 // x is GCD of n1 and n2
 // and x is GCD of n2 and (n1-n2) too
 // because n1 = (x * d1), n2 = (x * d2), so n1-n2 = x * (d1-d2)
 
-func greatestCommonDevider(n1 int, n2 int) int {
+func greatestCommonDivider(n1 int, n2 int) int {
 	if n2 > n1 {
-		return greatestCommonDevider(n2, n1)
+		return greatestCommonDivider(n2, n1)
 	}
 
 	if n2 == 0 { // when it's devisible, that mean n2 of the previous call (n1 this call) is GCD
 		return n1
 	}
-	return greatestCommonDevider(n2, n1%n2)
+	return greatestCommonDivider(n2, n1%n2)
 }
 
 func main() {
